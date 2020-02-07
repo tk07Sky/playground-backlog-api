1ef85e9447b87d86c6b2a193b9f45b40fd9de5d2
tk07Sky <recode.seadex777@gmail.com>
2020-02-08 02:25:16 +0900

    feat: issueの更新サンプルの作成

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,38 @@
+const fetch = require('node-fetch');
 const functions = require('firebase-functions');
+require('dotenv').config();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const Koa = require('koa')
+const app = new Koa();
+const Router = require('koa-router');
+const router = new Router();
+
+const baseUrl = process.env.BACKLOG_URL;
+const apiKeyParam = '?apiKey=' + process.env.API_KEY;
+ 
+router
+  .get('/issues/:issueId', async(ctx, next) => {
+    const url = baseUrl + '/api/v2/issues/' + ctx.params.issueId;
+    const res = await fetch(url + apiKeyParam);
+    const jsonRes = await res.json();
+    ctx.body = { res: jsonRes };
+  })
+  .post('/issues/finishedIssue/:issueId', async(ctx, next) => {
+    const url = baseUrl + '/api/v2/issues/' + ctx.params.issueId;
+    const body = 'statusId=4';
+    const options = {
+      method: 'PATCH',
+      body: body,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+    const res = await fetch(url + apiKeyParam, options);
+    const jsonRes = await res.json();
+    ctx.body = { res: jsonRes };
+  })
+  .post();
+
+app.use(router.routes()).use(router.allowedMethods());
+ 
+exports.api = functions.https.onRequest(app.callback());
